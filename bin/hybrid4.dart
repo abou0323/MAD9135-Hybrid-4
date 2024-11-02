2bac9fe32b7a427531682d92d0bed983affddea8
Sandra Abou-Eid <abou0323@algonquinlive.com>
2024-11-02 18:25:06 -0400

    separated fetch function

--- bin/hybrid4.dart
+++ bin/hybrid4.dart
@@ -4,28 +4,31 @@ import 'dart:convert' as convert;
 
 void main() async {
   try {
-    const url = "https://random-data-api.com/api/v2/users?size=";
-    const size = "10";
-    var uri = Uri.parse(url + size);
+    var data = await fetchUsers();
 
-    var response = await http.get(uri);
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body) as List<dynamic>;
-
-      var data =
-          List<Map<String, dynamic>>.from(jsonResponse); // List of Map objects
-
-      for (var user in data) {
-        var firstName = user["first_name"];
-        var lastName = user["last_name"];
-        var uid = user["uid"];
-        var userDetails = "uid: $uid, \t Full Name: $firstName $lastName";
-        print(userDetails);
-      }
-    } else {
-      print("Request failed with status: ${response.statusCode}.");
+    for (var user in data) {
+      var firstName = user["first_name"];
+      var lastName = user["last_name"];
+      var uid = user["uid"];
+      var userDetails = "uid: $uid, \t Full Name: $firstName $lastName";
+      print(userDetails);
     }
   } catch (err) {
     print("Failed to fetch. Error: $err");
   }
+}
+
+fetchUsers() async {
+  const url = "https://random-data-api.com/api/v2/users?size=";
+  const size = "10";
+  var uri = Uri.parse(url + size);
+
+  var response = await http.get(uri);
+  if (response.statusCode != 200) {
+    throw Exception("Error: ${response.statusCode}");
+  }
+  var jsonResponse = convert.jsonDecode(response.body) as List<dynamic>;
+  var data =
+      List<Map<String, dynamic>>.from(jsonResponse); // List of Map objects
+  return data;
 }
